1325f5d7559852c09b699a67cfebfefa493c5a65
liuzhichao <liuzhichao00@163.com>
2018-08-07 18:30:13 +0800

    Initial commit

---
+++ src/cn/sdut/Main.java
@@ -0,0 +1,13 @@
+package cn.sdut;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+    }
+
+}
