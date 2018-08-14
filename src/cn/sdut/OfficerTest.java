9aa08047a64df2c4b94989f5874b96f71ddafd59
liuzhichao <liuzhichao00@163.com>
2018-08-14 14:46:31 +0800

    构造方法中this的用法

--- src/cn/sdut/OfficerTest.java
+++ src/cn/sdut/OfficerTest.java
@@ -3,11 +3,12 @@ package cn.sdut;
 public class OfficerTest {
 
     public static void main(String[] args) {
-        Officer officer = new Officer();
+        /*Officer officer = new Officer();
         officer.setId(1001);
         officer.setName("cc");
         officer.setDept("Clerk");
-        officer.display();
+        officer.display();*/
+        Employee employee = new Employee();
     }
 
 }
