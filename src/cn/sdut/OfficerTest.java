202a76a83f7c5094399015f5f62bb0005fbb2c3f
liuzhichao <liuzhichao00@163.com>
2018-08-14 11:52:09 +0800

    继承:雇员和职员;重载:求和输出

---
+++ src/cn/sdut/OfficerTest.java
@@ -0,0 +1,15 @@
+package cn.sdut;
+
+public class OfficerTest {
+
+    public static void main(String[] args) {
+        Officer officer = new Officer();
+        officer.setId(1001);
+        officer.setName("cc");
+        officer.setDept("Clerk");
+        officer.display();
+    }
+
+}
+
+
