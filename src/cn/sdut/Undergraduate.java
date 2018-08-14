2e83656897b8e94d66505a83397558ebf627f456
liuzhichao <liuzhichao00@163.com>
2018-08-14 18:57:48 +0800

    学生类和大学生,构造方法的调用

---
+++ src/cn/sdut/Undergraduate.java
@@ -0,0 +1,25 @@
+package cn.sdut;
+
+public class Undergraduate extends Student {
+
+    private String specialty;//专业
+    private String grade;//年级
+
+    public Undergraduate() {
+
+    }
+
+    public Undergraduate(String name, int age, String specialty, String grade) {
+        super(name,age);
+        this.specialty = specialty;
+        this.grade = grade;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+}
