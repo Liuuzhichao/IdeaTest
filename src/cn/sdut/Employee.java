c3c679f24c1e811f8fb11f5aae82ad171d988458
liuzhichao <liuzhichao00@163.com>
2018-08-14 16:00:12 +0800

    重载的测试

--- src/cn/sdut/Employee.java
+++ src/cn/sdut/Employee.java
@@ -5,6 +5,22 @@ public class Employee {
     private int id;
     private String name;
 
+    public Employee () {
+        this(1001);
+        System.out.println("null");
+    }
+
+    public Employee ( int id ) {
+        this(id, "cc");//调用下面的构造器
+        System.out.println("part");
+    }
+
+    public Employee ( int id, String name ) {
+        this.id = id;
+        this.name = name;
+        System.out.println("all");
+    }
+
     public int getId() {
         return id;
     }
