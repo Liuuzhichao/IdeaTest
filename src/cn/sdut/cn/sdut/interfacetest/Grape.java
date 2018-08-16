b4ffc047e32daccbb79bf5517d2f6ee9e520b883
liuzhichao <liuzhichao00@163.com>
2018-08-16 18:30:12 +0800

    接口练习
    Signed-off-by: liuzhichao <liuzhichao00@163.com>

---
+++ src/cn/sdut/cn/sdut/interfacetest/Grape.java
@@ -0,0 +1,36 @@
+package cn.sdut.cn.sdut.interfacetest;
+
+/**
+ * Created by liuzhichao on 2018/8/16.
+ */
+public class Grape implements Fruit {
+
+    private int num;//颗粒数
+
+    public Grape() {
+        System.out.println("创建了一个葡萄对象");
+    }
+
+    public Grape(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void getFruit() {
+        System.out.println("get grapes");
+    }
+
+    @Override
+    public void waterFruit() {
+        System.out.println("water grapes");
+    }
+
+}
