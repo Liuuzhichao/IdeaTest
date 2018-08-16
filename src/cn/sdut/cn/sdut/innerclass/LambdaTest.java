49d8ade1c995b4866cc3b1f679a3f83fcbbff418
liuzhichao <liuzhichao00@163.com>
2018-08-16 16:36:00 +0800

    匿名内部类,Lambda表达式
    Signed-off-by: liuzhichao <liuzhichao00@163.com>

---
+++ src/cn/sdut/cn/sdut/innerclass/LambdaTest.java
@@ -0,0 +1,26 @@
+package cn.sdut.cn.sdut.innerclass;
+
+/**
+ * Created by liuzhichao on 2018/8/16.
+ */
+
+/**
+ * Lambda表达式:当作匿名方法使用
+ * 允许将行为传递到函数里
+ */
+public class LambdaTest {
+
+    public  static  void main(String[] args)
+    {
+        Ib  b=(i,j)->{
+            int sum=i+j;
+            System.out.println("ma..."+sum);};
+        b.ma(1,2);
+    }
+
+}
+
+interface Ib
+{
+    void ma(int i,int j);
+}
