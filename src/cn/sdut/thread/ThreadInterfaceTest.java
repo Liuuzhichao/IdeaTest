eefd661d8dc53693f1a2098505549463cef73aeb
liuzhichao <liuzhichao00@163.com>
2018-08-24 17:25:11 +0800

    temp
    Signed-off-by: liuzhichao <liuzhichao00@163.com>

---
+++ src/cn/sdut/thread/ThreadInterfaceTest.java
@@ -0,0 +1,30 @@
+package cn.sdut.thread;
+
+/**
+ * Created by liuzhichao on 2018/8/24.
+ */
+public class ThreadInterfaceTest {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MyThread1 t1 = new MyThread1();
+        Thread tt = new Thread(t1);
+        tt.start();//启动线程
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("In  main  i=" + i);
+        }
+    }
+
+}
+
+class MyThread1 implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("In  Run  i=" + i);
+        }
+    }
+
+}
