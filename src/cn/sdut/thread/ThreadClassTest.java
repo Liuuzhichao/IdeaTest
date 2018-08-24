64b3e853ace8e2ec863fe5b12825b4243aa3998e
liuzhichao <liuzhichao00@163.com>
2018-08-24 17:25:23 +0800

    temp
    Signed-off-by: liuzhichao <liuzhichao00@163.com>

---
+++ src/cn/sdut/thread/ThreadClassTest.java
@@ -0,0 +1,38 @@
+package cn.sdut.thread;
+
+/**
+ * Created by liuzhichao on 2018/8/24.
+ */
+public class ThreadClassTest {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MyThread t1 = new MyThread();
+        /**
+         * 启动线程是用start()方法,然后线程自动调用run()方法,但是线程并不是立马就运行,
+         * 而是进入就绪状态,等待CPU的调度才运行
+         */
+        t1.start();//启动线程
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("In  main  i=" + i);
+        }
+    }
+
+}
+
+
+class MyThread extends Thread {
+    //线程体，独立运行的程序片段
+
+    /**
+     * run()方法在线程启动后自动调用
+     */
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("In  Run  i=" + i);
+        }
+    }
+
+}
